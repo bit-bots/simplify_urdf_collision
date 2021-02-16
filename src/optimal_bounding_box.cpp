88c9d875be890fdbb7e2ce0390af1d4ba2946569
Jasper Güldenstein <5guelden@informatik.uni-hamburg.de>
2021-02-16 18:49:28 +0100

    cpp version

--- src/optimal_bounding_box.cpp
+++ src/optimal_bounding_box.cpp
@@ -45,14 +45,6 @@ std::pair<std::array<double, 16>, std::array<double,3>> createOptimalBoundingBox
   /* untested */
   Aff_transformation tf;
   CGAL::oriented_bounding_box(p, tf, CGAL::parameters::use_convex_hull(true));
-  std::array<double,16> tf_array;
-  for(int i = 0; i < 4; i++)
-  {
-    for(int j = 0; j < 4; j++)
-    {
-      tf_array[i*4+j] = tf.m(i,j);
-    }
-  }
   
   std::array<double, 6> bb_min_max;
   for(int i = 0; i < 3; i++)
@@ -61,7 +53,8 @@ std::pair<std::array<double, 16>, std::array<double,3>> createOptimalBoundingBox
     bb_min_max[i*2+1] = DBL_MAX;
 
   }
-
+  // rotate points such that the bounding is axis aligned 
+  // and get the minimum and maximum values for each axis
   std::array<Point, 8> obb_points_rotated;
   for(int i = 0; i < obb_points.size(); i++)
   {
@@ -73,16 +66,51 @@ std::pair<std::array<double, 16>, std::array<double,3>> createOptimalBoundingBox
     bb_min_max[4] = std::max(bb_min_max[4], obb_points_rotated[i].z());
     bb_min_max[5] = std::min(bb_min_max[5], obb_points_rotated[i].z());
   }
+
   std::array<double,3> bb_size;
   bb_size[0] =  bb_min_max[0] - bb_min_max[1]; 
   bb_size[1] =  bb_min_max[2] - bb_min_max[3];
   bb_size[2] =  bb_min_max[4] - bb_min_max[5];
-  /* untested end */
-  // Make a mesh out of the oriented bounding box
+  //std::cout << "size xyz: " << bb_size[0] <<  " " << bb_size[1] << " " << bb_size[1] << std::endl;
+
+  double offset_x = bb_size[0]/2 - bb_min_max[0];
+  double offset_y = bb_size[1]/2 - bb_min_max[2];
+  double offset_z = bb_size[2]/2 - bb_min_max[4];
+  //std::cout << "x_off: " << offset_x << " y_off: "  << offset_y << " z_off: " << offset_z << " " << std::endl;
+
+  // apply offset to each point of the bounding box for when it is written to file
+  Kernel::Vector_3 off(offset_x, offset_y, offset_z);
+  for(int i = 0; i<8; i++)
+  {
+    obb_points_rotated[i] += off;
+  }
+
+  // Create mesh, both rotated and not rotated and write each to a file
   Surface_mesh obb_sm;
   CGAL::make_hexahedron(obb_points[0], obb_points[1], obb_points[2], obb_points[3],
                         obb_points[4], obb_points[5], obb_points[6], obb_points[7], obb_sm);
   std::ofstream("box.off") << obb_sm;
+  Surface_mesh obb_sm_rotated;
+  CGAL::make_hexahedron(obb_points_rotated[0], obb_points_rotated[1], obb_points_rotated[2], obb_points_rotated[3],
+                        obb_points_rotated[4], obb_points_rotated[5], obb_points_rotated[6], obb_points_rotated[7], obb_sm_rotated);
+  std::ofstream("box_rotated.off") << obb_sm_rotated;
+
+  // put the values from the affine transformation matrix into a standard cpp array
+  std::array<double,16> tf_array;
+  for(int i = 0; i < 4; i++)
+  {
+    for(int j = 0; j < 4; j++)
+    {
+      tf_array[i*4+j] = tf.m(i,j);
+    }
+  }
+  // translation of the bounding box is not included in the affine transformation matrix
+  // therefore we add it here
+  Kernel::Vector_3 off_rotated = tf.inverse().transform(off);
+  tf_array[3] = -off_rotated.x();
+  tf_array[7] = -off_rotated.y();
+  tf_array[11] = -off_rotated.z();
+  
   return std::make_pair(tf_array, bb_size);
 }
 
@@ -121,13 +149,9 @@ int main(int argc, char** argv)
 {
   if(argc != 2)
   {
-      std::cerr << "Usage: " << argv[0] << " <input file.off> <output_file.off> (output optional, defaults to obb.off)" << std::endl;
+      std::cerr << "Usage: " << argv[0] << " <input file.off>" << std::endl;
       return EXIT_FAILURE;
   }
   auto a = createOptimalBoundingBox(argv[1]);
-  // todo do something with result
-  /*for(Point p : obb_points)
-  {
-    std::cout << p << std::endl;
-  }*/
+  // TODO do something with result
 } 
